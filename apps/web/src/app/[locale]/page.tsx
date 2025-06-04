80707688b1987e4bed4194442d96b561b3ae5931
Brian Luna <brian.damian.luna@gmail.com>
2025-06-04 22:10:31 +0000

    basic clerk integration

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,13 +1,14 @@
-import { useTranslations } from 'next-intl'
+import { getTranslations } from 'next-intl/server'
 
-export default function Home() {
-  const t = useTranslations()
+export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
+
+  const t = await getTranslations('home');
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl font-bold mb-4">{t('home.title')}</h1>
-        <p className="text-xl">{t('home.description')}</p>
+        <h1 className="text-4xl font-bold mb-4">{t('title')}</h1>
+        <p className="text-xl">{t('description')}</p>
       </div>
     </main>
   )
